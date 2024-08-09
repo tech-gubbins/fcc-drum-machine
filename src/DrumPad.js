3a8978c66db9b3eb2fe9e02c5dddbad4bea3972a
Joshua McNeel <joshmcneel@gmail.com>
2024-08-09 14:43:18 -0400

    Initial refactor from old project

---
+++ src/DrumPad.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react';
+
+const DrumPad = ({ padKey, id, sound, label, setDisplay, volume, power }) => {
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    if (!power) return;
+
+    const audio = audioRef.current;
+    if (audio) {
+      // Reset audio to start and manage volume
+      audio.currentTime = 0;
+      audio.volume = volume;
+
+      // Play audio and catch any errors
+      audio.play().catch((error) => {
+        console.error('Error playing sound:', error);
+      });
+
+      setDisplay(label); // Update the display with the sound label
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key.toUpperCase() === padKey) {
+        playSound();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [padKey, volume, power]);
+
+  return (
+    <div className="drum-pad" id={id} onClick={playSound}>
+      {padKey}
+      <audio ref={audioRef} id={padKey} src={`sounds/${sound}`} className="clip" />
+    </div>
+  );
+};
+
+export default DrumPad;
